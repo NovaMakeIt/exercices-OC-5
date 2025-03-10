bf7cf64f1a588f2652eba805009050b372daf5eb
NOVA <147254516+NovaMakeIt@users.noreply.github.com>
2025-03-10 17:58:38 +0100

    commit

--- Exercice10/index.js
+++ Exercice10/index.js
@@ -1,3 +1,18 @@
 //votre code ici
+function printNumbers(n) {
+
+    let result = "";
+
+    for (let i = 1; i <= n; i++) {
+        result += i;
+
+        // Ajouter un espace après chaque nombre sauf le dernier
+        if (i < n) {
+            result += " ";
+          }
+    }
+
+    return result;
+}
 
 export default printNumbers
